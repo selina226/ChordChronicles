af002a96c87fe44e7b556e4742c430143ccbfb1b
selina226 <166135201+selina226@users.noreply.github.com>
2024-05-29 05:01:11 +0000

    Commit

--- src/home.jsx
+++ src/home.jsx
@@ -18,7 +18,7 @@ function Home(){
         </section>
 
         <section className = "calendar-section">
-          <img src = './images/calendar_image.png' alt = 'Chord Chronicles Calendar' />
+          <img src = './public/calendar_image.png' alt = 'Chord Chronicles Calendar' />
         </section>
 
         <section className = "practice-section">
